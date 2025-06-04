99883f67f7faaa67c5db022980dfc92d205cac87
kr-MATAGI <dev_matagi@kakao.com>
2025-06-04 15:22:46 +0900

    [Feature] Graph Node/Edge 보여줄 수 있는 엔드포인트

--- fastapi_app/app/routers/status.py
+++ fastapi_app/app/routers/status.py
@@ -1,10 +1,13 @@
+import io
 from fastapi import (
     APIRouter,
     HTTPException,
 )
+from fastapi.responses import StreamingResponse
 
 from app.core.config import settings
 from app.routers.controller.res_ctl import ResController
+from app.routers.controller.llm_ctl import LLM_Controller
 
 router = APIRouter()
 res_controller = ResController()
@@ -32,3 +35,13 @@ async def get_res_monitor():
 @router.get("/res-threshold")
 async def get_res_threshold():
     return await res_controller.get_memory_alert()
+
+
+@router.get("/show-agent-graph")
+async def get_agent_graph():
+    # @TODO: 그래프 FastAPI로 보여줄 수 있는지 확인
+    buf = io.BytesIO()
+    graph_image = await LLM_Controller.show_graph()
+    buf.seek(0)
+
+    return StreamingResponse(buf, media_type="image/png")
